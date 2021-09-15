dbad3db9e927fcd050d089085f97314ed632cbac
KolX_2ayf <KolX_2ayf@13-25>
2021-09-15 14:43:46 +0300

    Lab(All new)

--- 15.09/Book.cpp
+++ 15.09/Book.cpp
@@ -5,19 +5,45 @@ using namespace std;
 
 Book::Book()
 {
-
+	this->name = nullptr;
+	this->author = nullptr;
+	this->genre = nullptr;
 }
 
 Book::Book(const char* name, const char* author, const char* genre)
 {
+	this->name = new char[strlen(name) + 1];
+	strcpy_s(this->name, strlen(name) + 1, name);
+
+	this->author = new char[strlen(author) + 1];
+	strcpy_s(this->author, strlen(author) + 1, author);
+
+	this->genre = new char[strlen(genre) + 1];
+	strcpy_s(this->genre, strlen(genre) + 1, genre);
 }
 
 Book::Book(const Book& b)
 {
+	this->name = new char[strlen(b.name) + 1];
+	strcpy_s(this->name, strlen(b.name) + 1, b.name);
+
+	this->author = new char[strlen(b.author) + 1];
+	strcpy_s(this->author, strlen(b.author) + 1, b.author);
+
+	this->genre = new char[strlen(b.genre) + 1];
+	strcpy_s(this->genre, strlen(b.genre) + 1, b.genre);
 }
 
 Book::~Book()
 {
+	delete[]this->name;
+	this->name = nullptr;
+
+	delete[]this->author;
+	this->author = nullptr;
+
+	delete[]this->genre;
+	this->genre = nullptr;
 }
 
 void Book::Print()
